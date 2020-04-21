4e69f5251d7fefee9051e9eaf7d2d272a0d3b213
Carol <carol.chiew@remarkgroup.com>
2020-04-21 20:38:08 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Brownie from './Brownie.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Brownie} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello, I'm Brownie from Malaysia
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.kyaroru.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Owner - Carol
         </a>
       </header>
     </div>
